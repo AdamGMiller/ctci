f206ce09bda155010ad5d86b01520fdd5ed245f6
AdamGMiller <adam@familyofadam.com>
2023-02-06 20:31:14 -0800

    Working on RotateMatrix

---
+++ citi.Tests/1.Strings/RotateMatrixTests.cs
@@ -0,0 +1,81 @@
+using ctci._1.Strings;
+using System.Drawing;
+
+namespace citi.Tests
+{
+    public partial class StringTests
+    {
+        [Fact]
+        public void RotateMatrixWorksWith2x2Matrix()
+        {
+            // Arrange
+            Color[,] inputMatrix = new Color[2, 2]  {
+                { Color.Red, Color.Blue },
+                { Color.Red, Color.Blue }
+            };
+            Color[,] expectedOutput = new Color[2, 2] {
+                { Color.Red, Color.Red },
+                { Color.Blue, Color.Blue }
+            };
+
+            var matrix = new Matrix();
+
+            // Act
+            var actualOutput = matrix.RotateMatrix(inputMatrix);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void RotateMatrixWorksWith3x3Matrix()
+        {
+            // Arrange
+            Color[,] inputMatrix = new Color[3, 3]  {
+                { Color.Red, Color.Blue, Color.Blue },
+                { Color.Green, Color.Yellow, Color.Blue },
+                { Color.Red, Color.Blue, Color.Blue },
+            };
+            Color[,] expectedOutput = new Color[3, 3] {
+                { Color.Red, Color.Green, Color.Red },
+                { Color.Blue, Color.Yellow, Color.Blue},
+                { Color.Blue, Color.Blue, Color.Blue },
+            };
+
+            var matrix = new Matrix();
+
+            // Act
+            var actualOutput = matrix.RotateMatrix(inputMatrix);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void RotateMatrixWorksWith4x4Matrix()
+        {
+            // Arrange
+            Color[,] inputMatrix = new Color[4, 4]  {
+                { Color.Red, Color.Blue, Color.Blue, Color.Black },
+                { Color.Green, Color.Yellow, Color.Blue, Color.Black },
+                { Color.Red, Color.Blue, Color.Blue, Color.Black },
+                { Color.Orange, Color.Orange, Color.Orange, Color.Orange },
+            };
+            Color[,] expectedOutput = new Color[4, 4] {
+                { Color.Orange, Color.Red, Color.Green,  Color.Red },
+                { Color.Orange, Color.Blue, Color.Yellow,  Color.Blue },
+                { Color.Orange, Color.Blue, Color.Blue,  Color.Blue },
+                { Color.Orange, Color.Black, Color.Black,  Color.Black },
+            };
+
+            var matrix = new Matrix();
+
+            // Act
+            var actualOutput = matrix.RotateMatrix(inputMatrix);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+    }
+}
---
+++ ctci/1.Strings/RotateMatrix.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+namespace ctci._1.Strings
+{
+    public class Matrix
+    {
+        public Color[,] RotateMatrix(Color[,] matrix)
+        {
+            var matrixSize = matrix.GetLength(0);
+            var outputMatrix = new Color[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int column = 0; column < matrixSize; column++)
+                {
+                    var value = matrix[row, column];
+                    var outputRow = matrixSize - column - 1;
+                    var outputColumn = matrixSize - row - 1;
+                    outputMatrix[outputRow, outputColumn] = value;
+
+                    // 2x2 matrix
+                    // if w = 0 && h = 0, w = matrixSize && h = 0
+                    // if w = 1 && h = 0, w = 1 && h = 1
+                    // if w = 0 && h = 1, w = 0 && h = 0
+                    // if w = 1 && h = 1, w = 0 && h = 1
+
+                    // 3x3 matrix
+                    // if w = 0 && h = 0, w = matrixSize && h = 0
+                    // if w = 1 && h = 0, w = matrixSize - 0 && h = matrixSize - 1
+                    // if w = 0 && h = 1, w = matrixSize - h && h = matrixSize - 0
+                    // if w = 1 && h = 1, w = 0 && h = 1
+
+
+                }
+            }
+
+            return outputMatrix;
+        }
+    }
+}
