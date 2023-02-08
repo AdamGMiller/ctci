5317307553cc8861683da7e87077cf5d2e48c98a
AdamGMiller <adam@familyofadam.com>
2023-02-08 07:04:54 -0800

    Zero matrix

---
+++ citi.Tests/1.Strings/ZeroMatrixTests.cs
@@ -0,0 +1,79 @@
+using ctci._1.Strings;
+
+namespace citi.Tests
+{
+    public partial class StringTests
+    {
+        [Fact]
+        public void ZeroMatrixWith3x3AndCenterZero()
+        {
+            // Arrange
+            int[,] inputMatrix = new int[3, 3]  {
+                { 1, 1, 1 },
+                { 1, 0, 1 },
+                { 1, 1, 1 },
+            };
+            int[,] expectedOutput = new int[3, 3]  {
+                { 1, 0, 1 },
+                { 0, 0, 0 },
+                { 1, 0, 1 },
+            };
+
+            var matrix = new ZeroMatrix();
+
+            // Act
+            var actualOutput = matrix.SetRowsAndColumnsToZeroIfElementIsZero(inputMatrix);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void ZeroMatrixWith3x3AndNoZeros()
+        {
+            // Arrange
+            int[,] inputMatrix = new int[3, 3]  {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+            };
+            int[,] expectedOutput = new int[3, 3]  {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+            };
+
+            var matrix = new ZeroMatrix();
+
+            // Act
+            var actualOutput = matrix.SetRowsAndColumnsToZeroIfElementIsZero(inputMatrix);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void ZeroMatrixWith3x3AndAllZeros()
+        {
+            // Arrange
+            int[,] inputMatrix = new int[3, 3]  {
+                { 0,0,0 },
+                { 0,0,0 },
+                { 0,0,0 },
+            };
+            int[,] expectedOutput = new int[3, 3]  {
+                { 0,0,0 },
+                { 0,0,0 },
+                { 0,0,0 },
+            };
+
+            var matrix = new ZeroMatrix();
+
+            // Act
+            var actualOutput = matrix.SetRowsAndColumnsToZeroIfElementIsZero(inputMatrix);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
---
+++ ctci/1.Strings/ZeroMatrix.cs
@@ -0,0 +1,61 @@
+﻿namespace ctci._1.Strings
+{
+    public class ZeroMatrix
+    {
+        public int[,] SetRowsAndColumnsToZeroIfElementIsZero(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            bool[] rowHasZeroElements = new bool[rows];
+            bool[] columnHasZeroElements = new bool[columns];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    var valueIsZero = matrix[row, column] == 0;
+
+                    if (valueIsZero)
+                    {
+                        rowHasZeroElements[row] = true;
+                        columnHasZeroElements[column] = true;
+                    }
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (rowHasZeroElements[row])
+                {
+                    SetRowToZero(row);
+                }
+            }
+
+            for (int column = 0; column < columns; column++)
+            {
+                if (columnHasZeroElements[column])
+                {
+                    SetColumnToZero(column);
+                }
+            }
+
+            return matrix;
+
+            void SetRowToZero(int column)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    matrix[row, column] = 0;
+                }
+            }
+
+            void SetColumnToZero(int row)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    matrix[row, column] = 0;
+                }
+            }
+        }
+    }
+}
