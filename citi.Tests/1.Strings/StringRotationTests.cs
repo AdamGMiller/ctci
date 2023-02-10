e88b7771a1cb585a2dd8238df79cd509dde1ea17
AdamGMiller <adam@familyofadam.com>
2023-02-10 06:55:08 -0800

    String rotation

---
+++ citi.Tests/1.Strings/StringRotationTests.cs
@@ -0,0 +1,26 @@
+using ctci._1.Strings;
+
+namespace citi.Tests
+{
+    public partial class StringTests
+    {
+        [Theory]
+        [InlineData("abcd", "cdab", true)]
+        [InlineData("waterbottle", "erbottlewat", true)]
+        [InlineData("abc", "ab", false)]
+        [InlineData("abcdefghijklmnopqrstuvwxyz", "vwxyzabcd", false)]
+        [InlineData("abc", "z", false)]
+        [InlineData("abc", "cba", false)]
+        public void StringRotationTests(string originalString, string possibleRotation, bool expectedIsRotation)
+        {
+            // Arrange
+            var stringRotation = new StringRotation();
+
+            // Act
+            var actualIsRotation = stringRotation.IsRotation(originalString, possibleRotation);
+
+            // Assert
+            Assert.Equal(expectedIsRotation, actualIsRotation);
+        }
+    }
+}
---
+++ ctci/1.Strings/StringRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace ctci._1.Strings
+{
+    public class StringRotation
+    {
+        public bool IsRotation(string originalString, string possibleRotation)
+        {
+            if (originalString.Length != possibleRotation.Length)
+            {
+                return false;
+            }
+
+            var doubledPossibleRotation = possibleRotation + possibleRotation;
+            return doubledPossibleRotation.Contains(originalString);
+        }
+    }
+}
