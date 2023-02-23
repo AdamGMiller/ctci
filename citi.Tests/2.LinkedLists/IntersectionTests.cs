bff18e3f6c559f2aff473851641e34110054663e
Adam Miller <adam.miller@healthcatalyst.com>
2023-02-23 15:57:51 -0800

    Intersection

---
+++ citi.Tests/2.LinkedLists/IntersectionTests.cs
@@ -0,0 +1,57 @@
+using ctci._2.LinkedLists;
+
+namespace citi.Tests._2.LinkedLists
+{
+    public partial class LinkedListsTests
+
+    {
+        [Fact]
+        public void IntersectionFoundCorrectly()
+        {
+            // Arrange
+            var intersection = new Intersection();
+            var firstNode = new Node(1);
+            firstNode.appendToTail(2);
+            firstNode.appendToTail(3);
+            firstNode.appendToTail(2);
+            firstNode.appendToTail(1);
+
+            var secondNode = new Node(1);
+            secondNode.appendToTail(2);
+            secondNode.next.next = firstNode.next.next;
+            secondNode.appendToTail(3);
+
+            var expectedResult = firstNode.next.next;
+
+            // Act
+            var actualResult = intersection.GetIntersectingNode(firstNode, secondNode);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void IntersectionNotFoundCorrectly()
+        {
+            // Arrange
+            var intersection = new Intersection();
+            var firstNode = new Node(1);
+            firstNode.appendToTail(2);
+            firstNode.appendToTail(3);
+            firstNode.appendToTail(2);
+            firstNode.appendToTail(1);
+
+            var secondNode = new Node(1);
+            secondNode.appendToTail(2);
+            secondNode.appendToTail(3);
+
+            var expectedResult = (Node?)null;
+
+            // Act
+            var actualResult = intersection.GetIntersectingNode(firstNode, secondNode);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ ctci/2.LinkedLists/Intersection.cs
@@ -0,0 +1,30 @@
+﻿namespace ctci._2.LinkedLists
+{
+    public class Intersection
+    {
+        public Node? GetIntersectingNode(Node firstNode, Node secondNode)
+        {
+            var nodes = new HashSet<Node>();
+            this.AddLinkedListAndGetIntersectionIfFound(firstNode, nodes);
+            return this.AddLinkedListAndGetIntersectionIfFound(secondNode, nodes);
+        }
+
+        private Node? AddLinkedListAndGetIntersectionIfFound(Node node, HashSet<Node> nodes)
+        {
+            var currentNode = node;
+            while (currentNode != null)
+            {
+                var success = nodes.Add(currentNode);
+
+                if (!success)
+                {
+                    return currentNode;
+                }
+
+                currentNode = currentNode.next;
+            }
+
+            return (Node?)null;
+        }
+    }
+}
