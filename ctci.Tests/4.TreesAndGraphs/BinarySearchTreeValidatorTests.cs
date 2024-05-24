3295a15fd82a8e0ffd8e4cea1e71829cd34118a1
Adam Miller <adam.miller@healthcatalyst.com>
2024-05-24 11:09:22 -0700

    Recursive binary search tree validator

---
+++ ctci.Tests/4.TreesAndGraphs/BinarySearchTreeValidatorTests.cs
@@ -0,0 +1,67 @@
+﻿using ctci._4.TreesAndGraphs;
+
+namespace citi.Tests._4.TreesAndGraphs
+{
+    public class BinarySearchTreeValidatorTests
+    {
+        [Fact]
+        public void ReturnsTrueWithPerfectBinarySearchTree()
+        {
+            // Arrange
+            var root = new TreeNode(10);
+            root.Left = new TreeNode(5);
+            root.Right = new TreeNode(15);
+            root.Left.Left = new TreeNode(1);
+            root.Left.Right = new TreeNode(7);
+            root.Right.Left = new TreeNode(12);
+            root.Right.Right = new TreeNode(16);
+
+            // Act
+            var validator = new ctci._4.TreesAndGraphs.BinarySearchTreeValidator(root);
+            var isValid = validator.IsValid();
+
+            // Assert
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void ReturnsTrueWithImperfectBinarySearchTree()
+        {
+            // Arrange
+            var root = new TreeNode(10);
+            root.Left = new TreeNode(5);
+            root.Right = new TreeNode(15);
+            root.Left.Left = new TreeNode(1);
+            root.Left.Right = new TreeNode(7);
+            root.Right.Left = null;
+            root.Right.Right = new TreeNode(16);
+
+            // Act
+            var validator = new ctci._4.TreesAndGraphs.BinarySearchTreeValidator(root);
+            var isValid = validator.IsValid();
+
+            // Assert
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void ReturnsFalseWithInvalidBinarySearchTree()
+        {
+            // Arrange
+            var root = new TreeNode(10);
+            root.Left = new TreeNode(5);
+            root.Right = new TreeNode(15);
+            root.Left.Left = new TreeNode(1);
+            root.Left.Right = new TreeNode(18);
+            root.Right.Left = new TreeNode(12);
+            root.Right.Right = new TreeNode(16);
+
+            // Act
+            var validator = new ctci._4.TreesAndGraphs.BinarySearchTreeValidator(root);
+            var isValid = validator.IsValid();
+
+            // Assert
+            Assert.False(isValid);
+        }
+    }
+}
---
+++ ctci/4.TreesAndGraphs/BinarySearchTreeValidator.cs
@@ -0,0 +1,43 @@
+﻿namespace ctci._4.TreesAndGraphs
+{
+    public class BinarySearchTreeValidator(TreeNode root)
+    {
+        private int largestValue = root.Data;
+
+        private bool isValid = true;
+
+        public bool IsValid()
+        {
+            var max = this.MaxOfChildNodes(root);
+            return this.isValid;
+        }
+
+        private int MaxOfChildNodes(TreeNode node)
+        {
+            if (this.isValid == false)
+            {
+                return Int32.MinValue;
+            }
+
+            var leftMax = (int?)null;
+            var rightMax = (int?)null;
+            if (node.Left != null)
+            {
+                leftMax = this.MaxOfChildNodes(node.Left);
+            }
+            if (node.Right != null)
+            {
+                rightMax = this.MaxOfChildNodes(node.Right);
+            }
+
+            if ((leftMax != null && leftMax > node.Data) || (rightMax != null && rightMax <= node.Data))
+            {
+                this.isValid = false;
+                return Int32.MinValue;
+            }
+
+            int[] values = [node.Data, leftMax ?? int.MinValue, rightMax ?? int.MinValue];
+            return values.Max();
+        }
+    }
+}
