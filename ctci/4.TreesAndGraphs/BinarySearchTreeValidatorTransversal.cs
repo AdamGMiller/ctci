eef3de147d9384f41f80629c9568d996e3a8d2c9
Adam Miller <adam.miller@outlook.com>
2024-05-25 06:43:03 -0700

    Binary Search Tree Validator using transversal

---
+++ ctci/4.TreesAndGraphs/BinarySearchTreeValidatorTransversal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace ctci._4.TreesAndGraphs
+{
+    public class BinarySearchTreeValidatorTransversal
+    {
+        private TreeNode root;
+
+        public BinarySearchTreeValidatorTransversal(TreeNode root)
+        {
+            this.root = root;
+        }
+
+        public bool IsValid()
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode currentNode = root;
+            TreeNode previousNode = null;
+
+            while (currentNode != null || stack.Count > 0)
+            {
+                while (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+
+                currentNode = stack.Pop();
+
+                if (previousNode != null && currentNode.Data <= previousNode.Data)
+                {
+                    return false;
+                }
+
+                previousNode = currentNode;
+                currentNode = currentNode.Right;
+            }
+
+            return true;
+        }
+    }
+}
